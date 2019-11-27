72205ec5d00b08658f4f0ba4bcec7520b21e9328
endeavour-fc <1669128906@qq.com>
2019-11-27 17:10:17 +0800

    C++

--- test4.cpp
+++ test4.cpp
@@ -24,6 +24,30 @@ class MyString
             strcat(temp.str,another.str);
             return temp;
         }
+        MyString& operator=(const MyString& another)
+        {
+            if(strcmp(this->str,another.str)==0)
+            {
+                return *this;
+            }
+            if(this->str==NULL)
+            {
+                this->len=another.len;
+                this->str=new char[this->len+1];
+                strcpy(this->str,another.str);
+                return *this;
+            }
+            else
+            {
+                delete []this->str;
+                this->str=new char [another.len+1];
+                this->len=another.len;
+                strcpy(this->str,another.str);
+                return *this;
+
+            }
+
+        }
         char* GetString()
         {
             return this->str;
@@ -36,7 +60,8 @@ int main()
 {
     MyString s1("hhhhhh");
     MyString s2("mmmmmm");
-    s1=s1+s2;
+    MyString s3;
+    s1= (s1+s2);
     cout<<s1.GetString()<<endl;
     return 0;
 }
