9855eb79245671ab69c0a51b14fd00dcba2313ee
endeavour-fc <1669128906@qq.com>
2019-11-30 14:39:49 +0800

    C++

--- test5.cpp
+++ test5.cpp
@@ -31,11 +31,23 @@ class Test
             }
             return *this;
         }
+        Test operator+(const Test& another)
+        {
+            Test temp(0);
+            temp.m_data=this->m_data+another.m_data;
+            return temp;
+        }
     private:
         int m_data;
 };
 int main()
 {
-    
+    Test t1(10);
+    Test t2(2);
+    Test t3;
+    t3=t1+t2;
+    cout<<t1.Getdata()<<endl;
+    cout<<t2.Getdata()<<endl;
+    cout<<t3.Getdata()<<endl;
     return 0;
 }
