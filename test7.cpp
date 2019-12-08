1eef4facdae558c64f80702fdf76b5564c2cc6d1
endeavour-fc <1669128906@qq.com>
2019-12-08 11:20:02 +0800

    C++

--- test7.cpp
+++ test7.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
 using namespace std;
 void insert_sort(int* arr,int len)
 {
@@ -55,6 +57,46 @@ void Quick_sort(int* arr,int start,int end)
         Quick_sort(arr,i+1,end);
     }
 }
+void Merge(int* arr,int start, int mid, int end,int* temp)
+{
+    int i_start=start;
+    int i_end=mid;
+    int j_start=mid+1;
+    int j_end=end;
+    int len=0;
+    while(i_start<=i_end&&j_start<=j_end)
+    {
+        if(arr[i_start]<arr[j_start])
+        {
+            temp[len++]=arr[i_start++];
+        }
+        else
+        {
+            temp[len++]=arr[j_start++];
+        }
+    }
+    while(i_start<=i_end)
+    {
+        temp[len++]=arr[i_start++];
+    }
+    while(j_start<=j_end)
+    {
+        temp[len++]=arr[j_start++];
+    }
+    for(int i=0;i<len;i++)
+    {
+        arr[start+i]=temp[i];
+    }
+}
+void MergeSort(int* arr,int start,int end,int* temp)
+{
+    if(start>=end)
+        return;
+    int mid=(start+end)/2;
+    MergeSort(arr,start,mid,temp);
+    MergeSort(arr,mid+1,end,temp);
+    Merge(arr,start,mid,end,temp);
+}
 void Print_sort(int* arr,int len)
 {
     for(int i=0;i<len;i++)
@@ -64,9 +106,12 @@ int main()
 {
     int arr[]={1,4,2,3,7,5,8,6,9,10};
     int len=sizeof(arr)/sizeof(arr[0]);
+    int* temp=(int*)malloc(len*sizeof(int));
+    MergeSort(arr,0,len-1,temp);
+    Print_sort(arr,len);
    // insert_sort(arr,len);
    // Shell_sort(arr,len);
-    Quick_sort(arr,0,len-1);
-    Print_sort(arr,len);
+   // Quick_sort(arr,0,len-1);
+   // Print_sort(arr,len);
     return 0;
 }
